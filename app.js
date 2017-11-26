535d5318b5f0d5cba7860603603bc792579c8c56
= <andrespechtbs@googlemail.com>
2017-11-26 22:03:00 +0100

    new feature project page

--- app.js
+++ app.js
@@ -25,12 +25,7 @@ app.use((req, res, next) => {
     }), next();
 });
 
-app.use((req,res,next)=>{
-    res.render('maintenance.hbs',{
-        pageTitle: "Maintenance",
-        information: "The site is currently not available"
-    })
-});
+
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
@@ -50,6 +45,13 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page'
+
+    });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
 
     });
 })
